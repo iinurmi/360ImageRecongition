9b8f1739d1659f383bc43d427c0423ca0bbf1ef7
Iiro Nurmi <iinurmi@gmail.com>
2020-11-07 14:24:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from numpy import asarray
+# load the image
+image = Image.open('image_koala.jpg')
+# convert image to numpy array
+data = asarray(image)
+print(type(data))
+# summarize shape
+print(data.shape)
+
+# create Pillow image
+image2 = Image.fromarray(data)
+print(type(image2))
+
+# summarize image details
+print(image2.mode)
+print(image2.size)
